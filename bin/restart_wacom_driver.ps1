e2d387bf9e9e691e101111a12d896fbc121cbc51
Lai, Yu-Hsuan <raincolee@gmail.com>
2021-08-10 18:21:37 +0800

    gsudo restart_wacom_driver.ps1

--- bin/restart_wacom_driver.ps1
+++ bin/restart_wacom_driver.ps1
@@ -1,8 +1,13 @@
 # PowerShell cmdlet to Restart the Wacom Drvier
+
+
 $srvName = "WTabletServicePro"
 $servicePrior = Get-Service $srvName
 "$srvName is now " + $servicePrior.status
-Set-Service $srvName -startuptype manual
-Restart-Service $srvName
+$cmd = @"
+Set-Service ${srvName} -startuptype manual
+Restart-Service ${srvName}
+"@
+gsudo $cmd
 $serviceAfter = Get-Service $srvName
 "$srvName is now " + $serviceAfter.status
